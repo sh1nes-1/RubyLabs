992ca1c655eee772d493ca1cd8f39671a4ae6d23
sh1nes-1 <kosovchychserhiy@gmail.com>
2021-11-18 16:44:32 +0200

    Fix nbsp

--- lab1/VideoCardParser.rb
+++ lab1/VideoCardParser.rb
@@ -14,13 +14,13 @@ class VideoCardParser
     max_price = item['max_price'].gsub(' ', '').strip
 
     memory_data = item['specs']["Пам'ять"].split(',') # 12ГБ, GDDR6, 15000МГц
-    memory_capacity  = memory_data[0].gsub(' ', '').strip
+    memory_capacity  = memory_data[0].gsub(' ', ' ').strip
     memory_type      = memory_data[1].gsub(' ', '').strip
-    memory_frequency = memory_data[2].gsub(' ', '').strip
+    memory_frequency = memory_data[2].gsub(' ', ' ').strip
 
     gpu_data = item['specs']['GPU'].split(',') # NVIDIA GeForce RTX 3060, 1777МГц
     gpu_name      = gpu_data[0].gsub(' ', '').strip
-    gpu_frequency = gpu_data[1].gsub(' ', '').strip
+    gpu_frequency = gpu_data[1].gsub(' ', ' ').strip
 
     VideoCard.new(title, memory_capacity, memory_frequency, memory_type, gpu_name, gpu_frequency, min_price, max_price, img_url, reference_url)
   rescue
